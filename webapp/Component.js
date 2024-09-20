ac951391a4d996ef5f27a2dca238cc97846bd121
Juan Pablo Torres Moreno <juan-pablo.torres@inetum.com>
2024-09-20 11:31:05 +0000

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.irvia.zreparaciones.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
